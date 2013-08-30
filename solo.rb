992a2ed3de603f2802e1e00e7369704b07eb0486
kenjiskywalker <git@kenjiskywalker.org>
2013-08-31 02:37:16 +0900

    solo.rbをENV[HOME]に変更

--- solo.rb
+++ solo.rb
@@ -1,6 +1,8 @@
+homedir = ENV["HOME"]
+
 file_cache_path           "/tmp/chef-solo"
-data_bag_path             "/Users/kenjiskywalker/mychefrepo/data_bags"
-encrypted_data_bag_secret "/Users/kenjiskywalker/mychefrepo/data_bag_key"
-cookbook_path             [ "/Users/kenjiskywalker/mychefrepo/site-cookbooks",
-                            "/Users/kenjiskywalker/mychefrepo/cookbooks" ]
-role_path                 "/Users/kenjiskywalker/mychefrepo/roles"
+data_bag_path             "#{homedir}/mychefrepo/data_bags"
+encrypted_data_bag_secret "#{homedir}/mychefrepo/data_bag_key"
+cookbook_path             [ "#{homedir}/mychefrepo/site-cookbooks",
+                            "#{homedir}/mychefrepo/cookbooks" ]
+role_path                 "#{homedir}/mychefrepo/roles"
