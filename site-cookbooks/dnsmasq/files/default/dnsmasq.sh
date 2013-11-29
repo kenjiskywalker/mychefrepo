d64c6052abccefcbd9ce4c9be56f74ab3a7e67dc
kenjiskywalker <git@kenjiskywalker.org>
2013-11-29 14:22:13 +0900

    ほぼ完璧

---
+++ site-cookbooks/dnsmasq/files/default/dnsmasq.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo launchctl unload /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
+sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
--- site-cookbooks/dnsmasq/recipes/default.rb
+++ site-cookbooks/dnsmasq/recipes/default.rb
@@ -7,10 +7,35 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package "dnsmasq"
+user = "kenjiskywalker"
+homedir = "/Users/kenjiskywalker"
+
+package "dnsmasq" do
+  notifies :run, "execute[cp-mxcl]"
+end
 
 template "/usr/local/etc/dnsmasq.conf" do
   source "dnsmasq_conf.erb"
   mode 0644
-  owner "kenjiskywalker"
+  owner "#{user}"
 end
+
+template "/etc/resolv.dnsmasq.conf" do
+  source "resolv_dnsmasq_conf.erb"
+  mode 0644
+  owner "#{user}"
+end
+
+execute "cp-mxcl" do
+  command "cp /usr/local/opt/dnsmasq/homebrew.mxcl.dnsmasq.plist  /Library/LaunchDaemons/"
+  action :nothing
+end
+
+cookbook_file "dnsmasq.sh" do
+  path "#{homedir}/bin/dnsmasq.sh"
+  action :create_if_missing
+  mode 0755
+  owner "#{user}"
+  only_if {File.exists?("#{homedir}/bin/")}
+end
+
